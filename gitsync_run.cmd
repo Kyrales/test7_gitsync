a4a8f66a0947b603b1ddb510a127bcc98f3bf6f7
Oleg Karataev <test@mail.ru>
2024-08-03 11:37:11 +0300

    Базовые настройки

---
+++ gitsync_run.cmd
@@ -0,0 +1,7 @@
+## Инициализация гитсинха
+
+gitsync init -u gitbot f:\otus\StorageTest_Git\ f:\otus\test7_gitsync\src\cf\
+
+## Синхронизация гита с хранилищем 1С
+
+gitsync sync -u gitbot f:\otus\StorageTest_Git\ f:\otus\test7_gitsync\src\cf\
